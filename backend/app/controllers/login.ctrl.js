85c5850eb67e5cb728d3001d6738524561010593
pyneninarendra@gmail.com <pyneninarendra@gmail.com>
2023-11-20 07:05:41 +0530

    bootstrap and fontawesome local installation with login, registration api

--- backend/app/controllers/login.ctrl.js
+++ backend/app/controllers/login.ctrl.js
@@ -1,14 +1,29 @@
 var loginCtrl = {}
+var registerModel = require('./../models/register.model')
+var security = require('./../utils/security')
 
-loginCtrl.get = function(req, res) {
+loginCtrl.get = function (req, res) {
     res.render('home', {
         view: 'login'
     })
 }
 
-loginCtrl.post = function(req, res) {
-    
+loginCtrl.post = function (req, res) {
+    checkUser(req, res)
 }
 
+checkUser = async (req, res) => {
+    var login = req.body
+    login.password = security.encryptByMd5(login.password)
+    let userInfo = await registerModel.findOne({email: login.email})
+    if(userInfo._id) {
+        userInfo.password === login.password? 
+        res.status(200).send(userInfo._id)
+        :
+        res.status(401).send('Password is incorrect')
+    } else {
+        res.status(401).send('User not found')
+    }
+}
 
 module.exports = loginCtrl
--- backend/app/controllers/register.ctrl.js
+++ backend/app/controllers/register.ctrl.js
@@ -1,13 +1,23 @@
 var registerCtrl = {}
+registerModel = require('./../models/register.model')
 
-registerCtrl.get = function(req, res) {
+registerCtrl.get = function (req, res) {
     res.render('home', {
         view: 'register'
     })
 }
 
-registerCtrl.post = function(req, res) {
-    
+registerCtrl.post = function (req, res) {
+    saveUser(req, res)
+}
+
+saveUser = async (req, res) => {
+    var user = new registerModel(req.body)
+    let userInfo = await user.save()
+    userInfo._id ? 
+    res.status(200).send(userInfo._id)
+    :
+    res.status(401).send('User Not Saved')
 }
 
 
